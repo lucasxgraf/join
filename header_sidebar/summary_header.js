8ce6cf4b0998b6506cfca528b8d8b9562e365e9c
213Leon213 <leon213@live.de>
2025-10-27 17:53:44 +0100

    toggle profilepicture + updated assets and header

--- header_sidebar/summary_header.js
+++ header_sidebar/summary_header.js
@@ -1,23 +1,30 @@
-window.addEventListener("DOMContentLoaded", closeDropDown);
+window.addEventListener("DOMContentLoaded", closeDropDownEvent);
 let dropDownOpen = false;
 
 function dropDown(event) {
 event.stopPropagation();
 let profilePic = document.getElementById('profilePicture');
 let header = document.getElementById('dropDownMenu');
+if (dropDownOpen) {
+   closeDropDown();
+} else {
 profilePic.style.backgroundColor = "#E1E6EC";
 header.style.display = "flex";
 dropDownOpen = true;
+} 
 }
 
 
+function closeDropDownEvent() { 
+document.body.addEventListener('click', closeDropDown); 
+}
+
 function closeDropDown() {
 let profilePic = document.getElementById('profilePicture');
-let menu = document.getElementById('dropDownMenu');    
-document.body.addEventListener('click', function () {
+let menu = document.getElementById('dropDownMenu');   
    if (dropDownOpen) {
       menu.style.display = "none";
       profilePic.style.backgroundColor = "";
-      } 
-   }); 
+      dropDownOpen = false;
+      }
 }
