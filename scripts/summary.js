370f1d5c98f57b22e1e93e62a186f87eac6cc49f
Lucas Graf <lucasgraf01@gmail.com>
2025-11-26 15:17:41 +0100

    delete files and first draft changes in styling

--- scripts/summary.js
+++ scripts/summary.js
@@ -1,3 +1,28 @@
+greetingTime();
+
+function greetingTime() {
+let greeting = document.getElementById('timeGreeting');
+const now = new Date();
+const hour = now.getHours();
+    switch (true) {
+        case (hour < 11):
+        greeting.textContent = "Good Morning,"        
+            break;
+        case (hour < 18):
+        greeting.textContent = "Good Afternoon,"        
+            break;
+        case (hour < 21):
+        greeting.textContent = "Good Evening,"        
+            break;
+        case (hour < 4):
+        greeting.textContent = "Good Night,"        
+            break;
+        default:
+            greeting.textContent = "Good Morning,"
+            break;
+    }
+}
+
 window.addEventListener("DOMContentLoaded", hoverTwoCardsEvent);
 window.addEventListener("DOMContentLoaded", hoverlongCard);
 window.addEventListener("DOMContentLoaded", resetlongCard);
